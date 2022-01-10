fe426329935e5febc43dde62138a5aaf2baf76b4
Doug Black Jr <dougblackjr@gmail.com>
2022-01-10 14:12:40 -0500

    Updating to support grids and bloqs

--- billboard/addon.setup.php
+++ billboard/addon.setup.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-	'author'         => 'tripleNERDscore',
-	'author_url'     => 'https://triplenerdscore.net/',
-	'name'           => 'Billboard',
-	'description'    => '',
-	'version'        => '1.0.0',
-	'namespace'      => 'TNS\Billboard',
-	'settings_exist' => false,
-	'fieldtypes'     => [
-		'billboard' => [
-			'compatibility' => 'text',
-		]
-	]
+    'author'         => 'tripleNERDscore',
+    'author_url'     => 'https://triplenerdscore.net/',
+    'name'           => 'Billboard',
+    'description'    => '',
+    'version'        => '1.0.1',
+    'namespace'      => 'TNS\Billboard',
+    'settings_exist' => false,
+    'fieldtypes'     => [
+        'billboard' => [
+            'compatibility' => 'text',
+        ]
+    ]
 ];
--- billboard/ft.billboard.php
+++ billboard/ft.billboard.php
@@ -1,87 +1,85 @@
 <?php
 
-class Billboard_ft extends EE_Fieldtype {
+class Billboard_ft extends EE_Fieldtype
+{
+    public $info = [
+        'name'      => 'Billboard',
+        'version'   => '1.0.1'
+    ];
 
-	var $info = array(
-		'name'		=> 'Billboard',
-		'version'	=> '1.0.0'
-	);
+    public $has_array_data = false;
 
-	var $has_array_data = false;
+    public function validate($data)
+    {
+        return true;
+    }
 
-	function validate($data)
-	{
-		return true;
-	}
+    public function display_field($data)
+    {
 
-	function display_field($data)
-	{
+        $settings = $this->settings['billboard_text_to_display'];
+        $el = '<div style="width:100%;">' . $settings . '</div>';
 
-		$settings = $this->settings['billboard_text_to_display'];
-		$el = '<div style="width:100%;">' . $settings . '</div>';
+        return $el;
+    }
 
-		return $el;
-	}
+    public function replace_tag($data, $params = '', $tagdata = '')
+    {
+        return null;
+    }
 
-	function replace_tag($data, $params = '', $tagdata = '')
-	{
-		return null;
-	}
+    /**
+     * Accept all content types.
+     *
+     * @param string  The name of the content type
+     * @param bool    Accepts all content types
+     */
+    public function accepts_content_type($name)
+    {
+        return ($name == 'channel' || $name == 'grid' || $name == 'bloqs/1');
+    }
 
-	/**
-	 * Accept all content types.
-	 *
-	 * @param string  The name of the content type
-	 * @param bool    Accepts all content types
-	 */
-	public function accepts_content_type($name)
-	{
-		return true;
-	}
+    public function display_settings($data)
+    {
+        ee()->load->model('addons_model');
 
-	function display_settings($data)
-	{
+        $format_options = ee()->addons_model->get_plugin_formatting(true);
 
-		ee()->load->model('addons_model');
+        $settings = array(
+            array(
+                'title' => 'Billboard Text',
+                'desc' => 'The HTML you want to display in the channel entry form',
+                'fields' => array(
+                    'billboard_text_to_display' => array(
+                        'type' => 'textarea',
+                        'value' => (!isset($data['billboard_text_to_display']) || $data['billboard_text_to_display'] == '') ? '' : $data['billboard_text_to_display'],
+                        'rows'  => 6,
+                    )
+                )
+            )
+        );
 
-		$format_options = ee()->addons_model->get_plugin_formatting(TRUE);
+        return array('field_options_billboard' => array(
+            'label' => 'field_options',
+            'group' => 'billboard',
+            'settings' => $settings
+        ));
+    }
 
-		$settings = array(
-			array(
-				'title' => 'Billboard Text',
-				'desc' => 'The HTML you want to display in the channel entry form',
-				'fields' => array(
-					'billboard_text_to_display' => array(
-						'type' => 'textarea',
-						'value' => ( ! isset($data['billboard_text_to_display']) OR $data['billboard_text_to_display'] == '') ? '' : $data['billboard_text_to_display'],
-						'rows'	=> 6,
-					)
-				)
-			)
-		);
+    public function grid_save_settings($data)
+    {
+        return array_merge($this->save_settings($data), $data);
+    }
 
-		return array('field_options_billboard' => array(
-			'label' => 'field_options',
-			'group' => 'billboard',
-			'settings' => $settings
-		));
-	}
+    public function save_settings($data)
+    {
+        return [
+            'billboard_text_to_display'  => ee()->input->post('billboard_text_to_display')
+        ];
+    }
 
-	function grid_save_settings($data)
-	{
-		return array_merge($this->save_settings($data), $data);
-	}
-
-	function save_settings($data)
-	{
-		return [
-	        'billboard_text_to_display'  => ee()->input->post('billboard_text_to_display')
-	    ];
-	}
-
-	public function update($version)
-	{
-		return true;
-	}
-
+    public function update($version)
+    {
+        return true;
+    }
 }
